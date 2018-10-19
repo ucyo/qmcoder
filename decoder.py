e58da680154bd29237cc636664910d5c06f1ff5d
ucyo <cayoglu@me.com>
2018-10-19 16:53:11 +0200

    Initial values for C and A

--- decoder.py
+++ decoder.py
@@ -6,4 +6,20 @@ Decoder class
 import numpy as np
 
 class Decoder(object):
-    pass
+
+    def __init__(self, table):
+        self.table = table
+        self.C     = np.uint32(0x0)
+        self.A     = np.uint32(0x10000)
+
+    @property
+    def Qe(self):
+        return np.uint16(self.table.qe)
+
+    @property
+    def Cx(self):
+        return np.bitwise_and(self.C, 0xFFFF0000)
+
+    @property
+    def Clow(self):
+        return np.bitwise_and(self.C, 0x0000FFFF)
