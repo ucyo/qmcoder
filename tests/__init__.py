7b044ef0c2472bfb2f1e96efe8eb2da42db68acd
ucyo <cayoglu@me.com>
2018-10-19 11:31:31 +0200

    Add better testing environment

---
+++ tests/__init__.py
@@ -0,0 +1,14 @@
+
+import os
+import bitstring as bs
+
+folder = os.path.dirname(__file__)
+
+with open(os.path.join(folder, 'test.raw'), 'rb') as f:
+    test = f.read()
+    test = [int(x) for x in bs.BitArray(test).bin]
+
+
+with open(os.path.join(folder, 'test.raw.compressed'), 'rb') as f:
+    expected = f.read()
+    expected = [int(x) for x in bs.BitArray(test).bin]
