e7adcf7cf3ef3554e8062560e419b3f52c66cf43
ucyo <cayoglu@me.com>
2018-10-22 11:24:18 +0200

    Finish decoding process by the book

--- decoder.py
+++ decoder.py
@@ -75,3 +75,45 @@ class Decoder(object):
         else:
             D = self.MPS
         return D
+
+    def cond_LPS_exchange(self):
+        if self.A < self.Qe:
+            D = self.MPS
+            # self.Cx -= self.A
+            self.C -= np.left_shift(self.A, 16)
+            self.A = self.Qe
+            self.estimate_qe_after_mps()
+        else:
+            D = 1 - self.MPS
+            # self.Cx -= self.A
+            self.C -= np.left_shift(self.A, 16)
+            self.A = self.Qe
+            self.estimate_qe_after_lps()
+        return D
+
+    def cond_MPS_exchange(self):
+        if self.A < self.Qe:
+            D = 1 - self.MPS
+            self.estimate_qe_after_lps()
+        else:
+            D = self.MPS
+            self.estimate_qe_after_mps()
+        return D
+
+    def renorm_d(self):
+        if self.CT == 0:
+            self.byte_in()
+            self.CT = 8
+        self.A = np.left_shift(self.A, 1)
+        self.C = np.left_shift(self.C, 1)
+        self.CT -= 1
+        if self.A < self.threequarter:
+            self.renorm_d()
+
+    def estimate_qe_after_lps(self):
+        if self.table.is_exchange_needed:
+            self.MPS = 1 - self.MPS
+        self.table.update_using_lps()
+
+    def estimate_qe_after_mps(self):
+        self.table.update_using_mps()
