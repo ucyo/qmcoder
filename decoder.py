c48e7f0b5e6edee1979fbefdb1fadf88c84b22ae
ucyo <cayoglu@me.com>
2018-10-22 09:31:04 +0200

    Add initialization process

--- decoder.py
+++ decoder.py
@@ -11,6 +11,15 @@ class Decoder(object):
         self.table = table
         self.C     = np.uint32(0x0)
         self.A     = np.uint32(0x10000)
+        self.BPST  = 0
+        self.BP    = self.BPST - 1
+
+        self.byte_in()
+        self.C = np.left_shift(self.C, 8)
+        self.byte_in()
+        self.C = np.left_shift(self.C, 8)
+        self.CT = 0
+
 
     @property
     def Qe(self):
